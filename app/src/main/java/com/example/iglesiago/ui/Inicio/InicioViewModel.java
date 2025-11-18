c0b3992495d1d64c7a98543aa704748554eff496
Jonathan Gabriel Garay <103967649+Jonise1564@users.noreply.github.com>
2025-11-18 10:32:32 -0300

    Update

--- app/src/main/java/com/example/iglesiago/ui/Inicio/InicioViewModel.java
+++ app/src/main/java/com/example/iglesiago/ui/Inicio/InicioViewModel.java
@@ -1,4 +1,4 @@
-package com.example.iglesiago.ui.gallery;
+package com.example.iglesiago.ui.Inicio;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
---
+++ app/src/main/java/com/example/iglesiago/ui/Ubicacion/UbicacionViewModel.java
@@ -0,0 +1,7 @@
+package com.example.iglesiago.ui.Ubicacion;
+
+import androidx.lifecycle.ViewModel;
+
+public class UbicacionViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
